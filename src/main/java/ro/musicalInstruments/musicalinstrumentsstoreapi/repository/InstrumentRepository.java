e75c7524795cf56fab14c88c267351fc5d5924f7
Sami <bmb,sami@yahoo.com>
2019-06-26 14:50:21 +0300

    extending JpaRepository with its methods

--- src/main/java/ro/musicalInstruments/musicalinstrumentsstoreapi/repository/InstrumentRepository.java
+++ src/main/java/ro/musicalInstruments/musicalinstrumentsstoreapi/repository/InstrumentRepository.java
@@ -1,5 +1,7 @@
 package ro.musicalInstruments.musicalinstrumentsstoreapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ro.musicalInstruments.musicalinstrumentsstoreapi.model.Instrument;
@@ -9,4 +11,6 @@ public interface InstrumentRepository extends JpaRepository<Instrument,Long> {
     @Query("Select instrument.instrument,instrument.brand from Instrument instrument")
     Instrument findByInstrumentAndBrand(String instrument,String brand);
 
+    @Query(value = "select * from Instrument where brand like 'Y%'",nativeQuery = true)
+    Page<Instrument> findByPartialBrand(Pageable pageable);
 }
