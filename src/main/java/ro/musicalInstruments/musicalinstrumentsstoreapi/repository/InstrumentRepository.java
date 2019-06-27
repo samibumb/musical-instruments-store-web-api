237638606fe8f53efd199bcff48890719c18a0e1
Sami <bmb,sami@yahoo.com>
2019-06-27 19:02:12 +0300

    extending jpaRepositry & using jpql/native query

--- src/main/java/ro/musicalInstruments/musicalinstrumentsstoreapi/repository/InstrumentRepository.java
+++ src/main/java/ro/musicalInstruments/musicalinstrumentsstoreapi/repository/InstrumentRepository.java
@@ -4,13 +4,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ro.musicalInstruments.musicalinstrumentsstoreapi.model.Instrument;
 
+@Repository
 public interface InstrumentRepository extends JpaRepository<Instrument,Long> {
 
+    //used the Java Persistence query language
     @Query("Select instrument.instrument,instrument.brand from Instrument instrument")
     Instrument findByInstrumentAndBrand(String instrument,String brand);
 
+    //used the native Query
     @Query(value = "select * from Instrument where brand like 'Y%'",nativeQuery = true)
     Page<Instrument> findByPartialBrand(Pageable pageable);
 }
